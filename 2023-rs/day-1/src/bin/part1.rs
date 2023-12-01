c54185b0ff26722b14789c6f66d6294b75339667
Christopher Zinda <christopher.zinda@posteo.de>
2023-12-01 06:52:26 +0100

    Added first solution

--- 2023-rs/day-1/src/bin/part1.rs
+++ 2023-rs/day-1/src/bin/part1.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world from part 1!");
+    let input = include_str!("input1.txt");
+    let result = input.split("\n").map(|line| {
+        if line.len() == 0 {
+            return 0;
+        }
+        let first_idx = line.find(char::is_numeric).unwrap();
+        let last_idx = line.rfind(char::is_numeric).unwrap();
+        let first_char = line.chars().nth(first_idx).unwrap().to_string();
+        let last_char = line.chars().nth(last_idx).unwrap().to_string();
+        let num_str = first_char + &last_char;
+        println!("num_str: {num_str}");
+        num_str.parse::<i32>().unwrap() 
+    }).sum::<i32>();
+    println!("Result: {result}");
 }
--- 2023-rs/day-1/src/bin/part2.rs
+++ 2023-rs/day-1/src/bin/part2.rs
@@ -1,3 +1,48 @@
 fn main() {
-    println!("Hello, world from part 2!");
+    let input = include_str!("input2.txt");
+    let result = input.split("\n").map(|line| {
+        if line.len() == 0 {
+            return 0;
+        }
+        let mut first_idx = line.len();
+        let mut last_idx = 0;
+        let mut first_num = 0;
+        let mut last_num = 0;
+        for num in 1..=9 {
+            let word = match num {
+                1 => "one",
+                2 => "two",
+                3 => "three",
+                4 => "four",
+                5 => "five",
+                6 => "six",
+                7 => "seven",
+                8 => "eight",
+                _ => "nine",
+            };
+            let mut word_idx = line.find(word);
+            if word_idx.is_some() && word_idx.unwrap() <= first_idx {
+                first_idx = word_idx.unwrap();
+                first_num = num;
+            }
+            let mut num_idx = line.find(&num.to_string());
+            if num_idx.is_some() && num_idx.unwrap() <= first_idx {
+                first_idx = num_idx.unwrap();
+                first_num = num;
+            }
+            word_idx = line.rfind(word);
+            if word_idx.is_some() && word_idx.unwrap() >= last_idx {
+                last_idx = word_idx.unwrap();
+                last_num = num;
+            }
+            num_idx = line.rfind(&num.to_string());
+            if num_idx.is_some() && num_idx.unwrap() >= last_idx {
+                last_idx = num_idx.unwrap();
+                last_num = num;
+            }
+        }
+        println!("{} {} {} {}", first_idx, first_num, last_idx, last_num);
+        first_num * 10 + last_num
+    }).sum::<i32>();
+    println!("Result: {result}");
 }
