9954a34d315be6c4df65e7b98c5321634591e504
Christopher Zinda <christopher.zinda@posteo.de>
2023-12-02 04:44:41 +0100

    Small improvements to C++ solution

--- 2023-cpp/day-1/part2.cpp
+++ 2023-cpp/day-1/part2.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <cassert>
+#include <cstdint>
 #include <fstream>
 #include <ranges>
 #include <string_view>
@@ -12,29 +13,28 @@ auto read_file(const std::string_view path) -> std::string {
     return input.substr(0, input.find_last_not_of('\n') + 1);
 }
 
-auto first_digit(const auto line) -> std::tuple<int, int> {
-    const auto first_digit = std::ranges::find_if(line, [](auto c) { return std::isdigit(c); });
-    return {std::distance(std::begin(line), first_digit), *first_digit - '0'};
+auto first_digit(const auto line) -> std::tuple<int64_t, int32_t> {
+    const auto first = *std::ranges::find_if(std::views::enumerate(line), [](auto c) { return std::isdigit(std::get<1>(c)); });
+    return {std::get<0>(first), std::get<1>(first) - '0'};
 }
 
 constexpr auto num_words =
     std::array{std::pair{"one"sv, 1}, std::pair{"two"sv, 2},   std::pair{"three"sv, 3}, std::pair{"four"sv, 4}, std::pair{"five"sv, 5},
                std::pair{"six"sv, 6}, std::pair{"seven"sv, 7}, std::pair{"eight"sv, 8}, std::pair{"nine"sv, 9}};
 
-auto index_of_word(const auto line, const std::string_view word, const auto transform) -> int {
-    const auto transformed_line = transform(line);
-    return static_cast<int>(std::distance(std::begin(transformed_line), 
-                                          std::begin(std::ranges::search(transformed_line, transform(word)))));
+auto index_of_word(const auto line, const std::string_view word, const auto transform) -> int64_t {
+    return std::get<0>(*std::begin(
+        std::ranges::search(std::views::enumerate(transform(line)), transform(word), [](auto a, auto b) { return std::get<1>(a) == b; })));
 }
 
-auto first_word(const auto line, const auto transform) -> std::tuple<int, int> {
-    const auto first_word = std::ranges::min_element(num_words, {}, 
-                                                     [&](const auto word) { return index_of_word(line, word.first, transform); });
-    const int index = index_of_word(line, first_word->first, transform);
+auto first_word(const auto line, const auto transform) -> std::tuple<int64_t, int32_t> {
+    const auto first_word =
+        std::ranges::min_element(num_words, {}, [&](const auto word) { return index_of_word(line, word.first, transform); });
+    const int64_t index = index_of_word(line, first_word->first, transform);
     return {index, first_word->second};
 }
 
-auto solve(const std::string_view input) -> int {
+auto solve(const std::string_view input) -> int32_t {
     return std::ranges::fold_left(std::views::split(input, '\n'), 0, [](auto sum, auto line) -> int {
         const auto [fw_idx, fw_val] = first_word(line, std::identity{});
         const auto [lw_idx, lw_val] = first_word(line, std::views::reverse);
